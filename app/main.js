8e793d0516b318ccdcd276c4a0b9f1cf14af6238
Yash Thakare <thakareyash74@gmail.com>
2024-12-22 21:32:00 +0530

    Stage 0: Initialize basic Git structure with main branch reference

---
+++ app/main.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const command = process.argv[2];
+
+switch (command) {
+    case "init":
+        createGitDirectory();
+        break;
+    default:
+        throw new Error(`Unknown command ${command}`);
+}
+
+function createGitDirectory() {
+    fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
+    fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
+    fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
+
+    fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
+    console.log("Initialized git directory");
+}
+
